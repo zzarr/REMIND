53977e1e919be91045fa1d41538fd6388e45a6cb
zzarr <azharsyah.curug@gmail.com>
2025-03-20 12:53:35 +0700

    up

--- resources/views/components/head-css.blade.php
+++ resources/views/components/head-css.blade.php
@@ -1,3 +1,3 @@
-<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('css/app2.css') }}" rel="stylesheet" type="text/css" />
